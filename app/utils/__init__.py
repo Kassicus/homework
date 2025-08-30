4dab4a1c48062c52fd1bcc91a6ab237a6ae2cfa7
Kassicus <kason.suchow@gmail.com>
2025-08-30 16:52:30 -0600

    a few more...

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,9 +1,30 @@
 """
 Utility functions for Contract Management Platform
 """
-from .decorators import admin_required, log_activity, validate_file_upload, handle_errors, cache_result, rate_limit
-from .helpers import format_file_size, format_currency, format_date, format_datetime, get_contract_status_display, get_contract_status_color, truncate_text
-from .validators import validate_file_extension, validate_file_size, validate_contract_data, validate_client_data, validate_user_data
+from .decorators import (
+    admin_required,
+    log_activity,
+    validate_file_upload,
+    handle_errors,
+    cache_result,
+    rate_limit,
+)
+from .helpers import (
+    format_file_size,
+    format_currency,
+    format_date,
+    format_datetime,
+    get_contract_status_display,
+    get_contract_status_color,
+    truncate_text,
+)
+from .validators import (
+    validate_file_extension,
+    validate_file_size,
+    validate_contract_data,
+    validate_client_data,
+    validate_user_data,
+)
 
 __all__ = [
     "validate_file_extension",
--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -69,8 +69,8 @@ def validate_file_upload(f):
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        from app.utils.validators import validate_file_extension, validate_file_size
         from app.utils.helpers import format_file_size
+        from app.utils.validators import validate_file_extension, validate_file_size
 
         # Check if file was uploaded
         if "contract_file" in request.files:
